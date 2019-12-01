6cc5911e15aadb396675e5b5fd426a0fdc26259f
guido732 <guido732@gmail.com>
2019-12-01 20:23:13 -0300

    Adds turnComputer function and general element selectors

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 const sequenceComputer = [];
 const sequenceUser = [];
-let rounds = 0;
+let round = 0;
+
+const $messages = document.querySelector("#messages");
+const $roundCounter = document.querySelector("#round-counter");
 
 document.querySelector("#start-button").onclick = () => {
-	blockUserInput();
+	lockUserInput();
+	turnComputer();
 };
 
+function handleUserInput(e) {}
+
+function turnComputer() {
+	round++;
+	updateText(`Round N# ${round}`, $roundCounter);
+	updateText("Computer's turn!", $messages);
+
+	let selector = Math.floor(Math.random() * 4);
+	console.log(selector);
+}
+
+function updateText(textToUpdate, elementToUpdate) {
+	elementToUpdate.innerHTML = textToUpdate;
+}
+
 function lockUserInput() {
-	document.querySelectorAll("color-button").forEach($button => {
+	document.querySelectorAll("color-button").forEach(($button, index) => {
 		$button.onclick = function() {};
 	});
 }
+
 function unlockUserInput() {
 	document.querySelectorAll("color-button").forEach($button => {
 		$button.onclick = handleUserInput();
 	});
 }
-
-function handleUserInput(e) {}
